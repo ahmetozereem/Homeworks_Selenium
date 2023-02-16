8a0cc7d4b256506b60586d44cc553c15535109c1
ahmetozereem <ahmetozer.eem@gmail.com>
2023-02-16 20:11:58 +0100

    5. commit

--- Homework4/src/test/java/pages/TableTestsPages.java
+++ Homework4/src/test/java/pages/TableTestsPages.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class TableTestsPages {
     public TableTestsPages() {
         PageFactory.initElements(Driver.getDriver(), this);
@@ -12,44 +14,11 @@ public class TableTestsPages {
     @FindBy(xpath = "(//td[text()='http://www.timconway.com'])[1]") // for first test
     public WebElement table4And5;
 
-    @FindBy (css = "#table1[class='tablesorter'] thead tr th")
-    public WebElement titleOfFirstTitleOfFirstTableList;
+    @FindBy (css = "#table1[class='tablesorter'] thead tr th") // for second test (Table 1)
+    public List<WebElement> table1Elements;
 
-    @FindBy (xpath = "(//table[@id='table1']/thead/tr/th)[1]")
-    public WebElement titleOfFirstTitleOfFirstTable;
-
-    @FindBy (xpath = "(//table[@id='table1']/thead/tr/th)[2]")
-    public WebElement titleOfsecondTitleOfFirstTable;
-
-    @FindBy (xpath = "(//table[@id='table1']/thead/tr/th)[3]")
-    public WebElement titleOfThirdTitleOfFirstTable;
-
-    @FindBy (xpath = "(//table[@id='table1']/thead/tr/th)[4]")
-    public WebElement titleOfFourthTitleOfFirstTable;
-
-    @FindBy (xpath = "(//table[@id='table1']/thead/tr/th)[5]")
-    public WebElement titleOfFifthTitleOfFirstTable;
-
-    @FindBy (xpath = "(//table[@id='table1']/thead/tr/th)[6]")
-    public WebElement titleOfSixthTitleOfFirstTable;
-
-    @FindBy (xpath = "(//table[@id='table2']/thead/tr/th)[1]")
-    public WebElement titleOfFirstTitleOfSecondTable;
-
-    @FindBy (xpath = "(//table[@id='table2']/thead/tr/th)[2]")
-    public WebElement titleOfsecondTitleOfSecondTable;
-
-    @FindBy (xpath = "(//table[@id='table2']/thead/tr/th)[3]")
-    public WebElement titleOfThirdTitleOfSecondTable;
-
-    @FindBy (xpath = "(//table[@id='table2']/thead/tr/th)[4]")
-    public WebElement titleOfFourthTitleOfSecondTable;
-
-    @FindBy (xpath = "(//table[@id='table2']/thead/tr/th)[5]")
-    public WebElement titleOfFifthTitleOfSecondTable;
-
-    @FindBy (xpath = "(//table[@id='table2']/thead/tr/th)[6]")
-    public WebElement titleOfSixthTitleOfSecondTable;
+    @FindBy (css = "#table2[class='tablesorter'] thead tr th") // for second test (Table 2)
+    public List<WebElement> table2Elements;
 
     @FindBy (xpath = "(//td[@class='dues'])[3]") // for third test
     public WebElement valueOfSecondTable;
--- Homework4/src/test/java/tests/TableTests.java
+++ Homework4/src/test/java/tests/TableTests.java
@@ -24,61 +24,15 @@ public class TableTests extends TestBase {
 
     @Test (priority = -100)
     public void questionTwoTest (){
-
-        /* Birinci klasik yöntem
-
-        List<WebElement> listOfWebElementsOfTable1 = new ArrayList<>();
+        List<WebElement> listOfWebElementsOfTable1 = tableTestsPages.table1Elements;
         List<String> listOftitlesOfFirstTable = new ArrayList<>();
-
-        listOfWebElementsOfTable1.add(tableTestsPages.titleOfFirstTitleOfFirstTable);
-        listOfWebElementsOfTable1.add(tableTestsPages.titleOfsecondTitleOfFirstTable);
-        listOfWebElementsOfTable1.add(tableTestsPages.titleOfThirdTitleOfFirstTable);
-        listOfWebElementsOfTable1.add(tableTestsPages.titleOfFourthTitleOfFirstTable);
-        listOfWebElementsOfTable1.add(tableTestsPages.titleOfFifthTitleOfFirstTable);
-        listOfWebElementsOfTable1.add(tableTestsPages.titleOfSixthTitleOfFirstTable);
-
-            for (int i=0;i<6;i++) {
-                listOftitlesOfFirstTable.add(listOfWebElementsOfTable1.get(i).getText());
-        }
-            for ( String  a : listOftitlesOfFirstTable) {
-            System.out.println(a);
-
-        }
-
-        List<WebElement> listOfWebElementsOfTable2 = new ArrayList<>();
-        List<String> listOftitlesOfSecondTable = new ArrayList<>();
-
-        listOfWebElementsOfTable2.add(tableTestsPages.titleOfFirstTitleOfSecondTable);
-        listOfWebElementsOfTable2.add(tableTestsPages.titleOfsecondTitleOfSecondTable);
-        listOfWebElementsOfTable2.add(tableTestsPages.titleOfThirdTitleOfSecondTable);
-        listOfWebElementsOfTable2.add(tableTestsPages.titleOfFourthTitleOfSecondTable);
-        listOfWebElementsOfTable2.add(tableTestsPages.titleOfFifthTitleOfSecondTable);
-        listOfWebElementsOfTable2.add(tableTestsPages.titleOfSixthTitleOfSecondTable);
-
-            for (int i=0;i<6;i++) {
-            listOftitlesOfSecondTable.add(listOfWebElementsOfTable2.get(i).getText());
-        }
-            for ( String  b : listOftitlesOfSecondTable) {
-            System.out.println(b);
-        }
-
-            for (int i=0;i<6;i++) {
-            Assert.assertEquals(listOftitlesOfFirstTable.get(i), listOftitlesOfSecondTable.get(i));
-        }
-
-         */
-
-        // ikinci yöntem
-
-        List<WebElement> listOfWebElementsOfTable1 = Driver.getDriver().findElements(By.cssSelector("#table1[class=\"tablesorter\"] thead tr th"));
-        List<String> listOftitlesOfFirstTable = new ArrayList<>();
-        List<WebElement> listOfWebElementsOfTable2 = Driver.getDriver().findElements(By.cssSelector("#table2[class=\"tablesorter\"] thead tr th"));
+        List<WebElement> listOfWebElementsOfTable2 = tableTestsPages.table2Elements;
         List<String> listOftitlesOfSecondTable = new ArrayList<>();
 
         for (int i=0;i<6;i++) {
             listOftitlesOfFirstTable.add(listOfWebElementsOfTable1.get(i).getText());
             listOftitlesOfSecondTable.add(listOfWebElementsOfTable2.get(i).getText());
-            //System.out.println(listOftitlesOfFirstTable.get(i) +"="+listOftitlesOfSecondTable.get(i));
+            System.out.println(listOftitlesOfFirstTable.get(i) +"="+listOftitlesOfSecondTable.get(i));
             Assert.assertEquals(listOftitlesOfFirstTable.get(i), listOftitlesOfSecondTable.get(i));
         }
 
