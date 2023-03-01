6e49fa67e23d3ce2fe040db60204b7b81c254d7c
ahmetozereem <ahmetozer.eem@gmail.com>
2023-03-01 15:56:45 +0100

    second commit

--- CucumberHomework2/test-output-thread/report.js
+++ CucumberHomework2/test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"c57f8f51-2f43-454a-9c5f-d71d97c9a425","feature":"Kullanici sepete bir urun ekler ve sepet simgesi uzerinde bu urun sayisindaki artisi görur","scenario":"Sepete bir urun eklenince sepet simgesi uzerindeki sayi bir artsin","start":1677680860579,"group":1,"content":"","tags":"@homework,","end":1677680884662,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"44ec8b68-0b82-463e-a27b-cf237ff70dd3","feature":"Kullanici sepete bir urun ekler ve sepet simgesi uzerinde bu urun sayisindaki artisi görur","scenario":"Sepete bir urun eklenince sepet simgesi uzerindeki sayi bir artsin","start":1677682545245,"group":1,"content":"","tags":"@homework,","end":1677682572476,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
