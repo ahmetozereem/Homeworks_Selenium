bc3633722ddabe8b7a1444e89b3b3e5000655b89
ahmetozereem <ahmetozer.eem@gmail.com>
2023-03-02 19:16:42 +0100

    third commit

--- CucumberHomework2/target/test-classes/Homework2.feature
+++ CucumberHomework2/target/test-classes/Homework2.feature
@@ -7,7 +7,6 @@ Feature: Kullanici sepete bir urun ekler ve sepet simgesi uzerinde bu urun sayis
   Scenario: Sepete bir urun eklenince sepet simgesi uzerindeki sayi bir artsin
 
     When Kullanici Cookies ile ilgili onayi kabul eder
-    And Kullanici Cookies ile ilgili onayi kabul eder
     And Kullanici ust menu bardan Pricing linkine tiklar
     And Kullanici bir paketi buy now butonu ile sepete ekler
     And Kullanici ana sayfaya tekrar geri gider
--- CucumberHomework2/test-output-thread/report.js
+++ CucumberHomework2/test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"44ec8b68-0b82-463e-a27b-cf237ff70dd3","feature":"Kullanici sepete bir urun ekler ve sepet simgesi uzerinde bu urun sayisindaki artisi görur","scenario":"Sepete bir urun eklenince sepet simgesi uzerindeki sayi bir artsin","start":1677682545245,"group":1,"content":"","tags":"@homework,","end":1677682572476,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"d4bc58c5-10fb-4c52-b57b-595ea1d3ff30","feature":"Kullanici sepete bir urun ekler ve sepet simgesi uzerinde bu urun sayisindaki artisi görur","scenario":"Sepete bir urun eklenince sepet simgesi uzerindeki sayi bir artsin","start":1677780888125,"group":1,"content":"","tags":"@homework,","end":1677780911241,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
